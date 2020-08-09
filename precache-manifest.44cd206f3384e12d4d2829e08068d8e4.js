c17cb72b4802f113ea6030f7cf8aab9663d2aae7
Michael <michelo851a1203@gmail.com>
2020-08-09 09:46:44 +0800

    deploy

---
+++ precache-manifest.44cd206f3384e12d4d2829e08068d8e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4a3b37862f861123c699da79195dff",
+    "url": "/dragball-project/index.html"
+  },
+  {
+    "revision": "6574d1fa3d4d4c0b2840",
+    "url": "/dragball-project/js/app.7554340e.js"
+  },
+  {
+    "revision": "59490dfbed94e6fb1dc4",
+    "url": "/dragball-project/js/chunk-vendors.808f8016.js"
+  },
+  {
+    "revision": "cf1ec32d419f4d2d9fe930d7d36db7ad",
+    "url": "/dragball-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dragball-project/robots.txt"
+  }
+]);
