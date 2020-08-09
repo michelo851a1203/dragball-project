a85792c94beeb2fbd92f488f575bdfbb8a134e6b
Michael <michelo851a1203@gmail.com>
2020-08-09 09:36:58 +0800

    fux test

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === "production"
-        ? "dragball-project"
+        ? "/"
+        // ? "dragball-project"
         : "/",
     // 如果出現錯誤 : Error: No module factory available for dependency type: CssDependency ，就加這個
     css: {
